f0db0828f543163bf2abb8d20dfa26eaafa80450
cartolari <bruno.cartolari@gmail.com>
2014-10-21 16:39:58 -0200

    Add expectation for not requiring confirmation when a use logs trough OAuth

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -1,11 +1,15 @@
 describe Users::OmniauthCallbacksController do
   context "with the authentcation info" do
-    let(:user) { double('user', skip_confirmation!: nil, save: nil) }
+    let(:user) { double('user', save: nil) }
     before { expect(Users::OmniauthUser).to receive(:new).and_return(double(build_user: user))}
     before { expect(subject).to receive(:omniauth_info) }
+
     it "should authenticate and sign in a user" do
       expect(subject).to receive(:sign_in_and_redirect).with(user, event: :authentication)
+      expect(user).to receive(:skip_confirmation!)
+
       subject.authenticate_user
     end
+
   end
 end
