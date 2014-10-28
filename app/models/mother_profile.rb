24161e2bc70d275adbe0b58d436e752130254b86
cartolari <bruno.cartolari@gmail.com>
2014-10-28 19:11:52 -0200

    Validation for required children for mothers

--- app/models/mother_profile.rb
+++ app/models/mother_profile.rb
@@ -5,6 +5,16 @@ class MotherProfile < ActiveRecord::Base
   validates_acceptance_of :is_mother, unless: -> { is_mother? or is_pregnant? }
   validates_acceptance_of :is_pregnant, unless: -> { is_mother? or is_pregnant? }
   validates_presence_of :profile
+  validate :mother_has_children
 
   accepts_nested_attributes_for :children, allow_destroy: true
+
+  def mother_has_children
+    if is_mother?
+      current_children = children.select{|c| !c.marked_for_destruction? }
+      unless current_children.to_a.size > 0
+        errors.add(:base, I18n.t('activerecord.errors.models.mother_profile.mother_has_no_children'))
+      end
+    end
+  end
 end
--- spec/factories/mother_profile.rb
+++ spec/factories/mother_profile.rb
@@ -1,12 +1,13 @@
 FactoryGirl.define do
   factory :mother_profile do
-    is_mother true
-    is_pregnant false
+    is_mother false
+    is_pregnant true
     profile
   end
 
   trait :mother do
     is_mother true
+    children { create_list :child, 2 }
   end
 
   trait :pregnant do
@@ -23,5 +24,13 @@ FactoryGirl.define do
 
   trait :without_profile do
     profile nil
+  end
+
+  trait :with_children do
+    children { create_list :child, 2 }
+  end
+
+  trait :without_children do
+    children []
   end
 end
