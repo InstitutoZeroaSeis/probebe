6853042439c3fff09746d1e87acda86e13b730c7
cartolari <bruno.cartolari@gmail.com>
2014-10-30 20:19:39 -0200

    Add more context to the message sender spec

--- spec/models/message_sender_spec.rb
+++ spec/models/message_sender_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 RSpec.describe MessageSender, :type => :model do
 
-  context "with a born, male and half an year old child" do
-    subject { MessageSender.new.send_messages }
-    before { create(:child, :born, birth_date: 24.weeks.ago) }
-    before { create(:message, :male, maximum_valid_week: 24, baby_target_type: 'born') }
-    it { is_expected.to match_array([MessageDelivery.new(Message.first, [Child.first])]) }
+  context "with a message targeting born, males until 24 weeks" do
+    context "and with a born, male and half an year old child" do
+      subject { MessageSender.new.send_messages }
+      before { create(:child, :born, birth_date: 24.weeks.ago) }
+      before { create(:message, :male, maximum_valid_week: 24, baby_target_type: 'born') }
+      it { is_expected.to match_array([MessageDelivery.new(Message.first, [Child.first])]) }
+    end
   end
 
 end
