12d9cd39e7b62ba0f3edb229cf2bb3ad22b47708
cartolari <bruno.cartolari@gmail.com>
2014-10-31 17:27:55 -0200

    WIP: Refactor message sender

--- app/models/message_sender.rb
+++ app/models/message_sender.rb
@@ -1,38 +1,19 @@
 class MessageSender
+  attr_reader :messages
+
+  def initialize(messages)
+    @messages = messages
+  end
 
   def send_messages
-    Message.all.map do |message|
-      delivery = MessageDelivery.new(message)
-      delivery.children = find_childrens_for_message(message)
-      delivery
-    end
   end
 
+  def get_profiles
+    messages.each do |message|
+      profile_finder = ProfileFinder.new(message)
+      profile_finder.find_profiles_by_message
+    end
+  end
   protected
 
-  def find_childrens_for_message(message)
-    children = filter_by_born_state(message)
-    children = filter_by_gender(message, children)
-    children = filter_by_age(message, children)
-  end
-
-  def filter_by_born_state(message, children=Child.all)
-    Child.where(born: message.pregnancy? ? false: true)
-  end
-
-  def filter_by_gender(message, children=Child.all)
-    if message.gender != 'both'
-      children.send(message.gender)
-    else
-      children
-    end
-  end
-
-  def filter_by_age(message, children)
-    children.to_a.select do |child|
-      min_week = message.minimum_valid_week || 0
-      max_week = message.maximum_valid_week || Message::MAXIMUM_POSSIBLE_WEEK
-      (min_week..max_week).include? child.age_in_weeks
-    end
-  end
 end
