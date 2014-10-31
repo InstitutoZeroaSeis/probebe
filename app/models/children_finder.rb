35abe1f9713685830f4c89a1abf755d678363a68
cartolari <bruno.cartolari@gmail.com>
2014-10-31 16:02:05 -0200

    Refactor children finder

---
+++ app/models/children_finder.rb
@@ -0,0 +1,42 @@
+class ChildrenFinder
+  attr_reader :children
+  attr_reader :message
+
+  def initialize(message, children)
+    @message = message
+    @children = children
+  end
+
+  def find_childrens_for_message
+    children = filter_by_born_state(@children)
+    children = filter_by_gender(children)
+    filter_by_age(children)
+  end
+
+  def any_children_found?
+    find_childrens_for_message.size > 0
+  end
+
+  protected
+
+  def filter_by_born_state(children)
+    children.where(born: @message.pregnancy? ? false: true)
+  end
+
+  def filter_by_gender(children)
+    if @message.gender != 'both'
+      children.send(@message.gender)
+    else
+      children
+    end
+  end
+
+  def filter_by_age(children)
+    children.to_a.select do |child|
+      min_week = @message.minimum_valid_week || 0
+      max_week = @message.maximum_valid_week || Message::MAXIMUM_POSSIBLE_WEEK
+      (min_week..max_week).include? child.age_in_weeks
+    end
+  end
+
+end
---
+++ app/models/profile_finder.rb
@@ -0,0 +1,15 @@
+class ProfileFinder
+  attr_reader :message
+
+  def initialize(message)
+    @message = message
+  end
+
+  def find_profiles_by_message
+    MotherProfile.select do |profile|
+      children_finder = ChildrenFinder.new(message, profile.children)
+      children_finder.any_children_found?
+    end
+  end
+
+end
