f7b8fe7862e9c1745a922efa94c4e4506193ab1f
cartolari <bruno.cartolari@gmail.com>
2014-11-03 11:53:17 -0200

    Fix seed data

--- db/sample_data.rb
+++ db/sample_data.rb
@@ -10,22 +10,22 @@ ActiveRecord::Base.transaction do
   user.skip_confirmation!
   user.save!
   profile = user.create_profile!(first_name: 'Eri', last_name: 'Jonen', gender: 'female', is_mother: true, is_pregnant: true,
-                                 pregnancy_start_date: 3.months.ago,
+                                 pregnancy_start_date: 6.months.ago,
                                  children_attributes: [{ name: 'Joana', gender: 'female', birth_date: (1.years + 6.months).ago }],
                                  phones_attributes: [{area_code: '11', number: '87654321', phone_type: 'dumbphone'}])
 
   prevention_category = Category.create!(name: 'Prevenção', parent_category: Category.create!(name: 'Saúde'))
   savings_category = Category.create!(name: 'Poupar', parent_category: Category.create!(name: 'Financeira'))
 
-  # Bebe da francisca
+  # Bebe da Francisca
   Message.create!(text: 'Mensagem para bebê nascido do sexo masculino entre 5 e 7 meses', gender: 'male', baby_target_type: 'born', category: prevention_category, minimum_valid_week: 20, maximum_valid_week: 30)
-  # Bebe em gestação da Eri
+  # Gestação da Eri
   Message.create!(text: 'Mensagem para bebê em gestação do sexo masculino de 5 a 7 meses', gender: 'male', baby_target_type: 'pregnancy', category: savings_category, minimum_valid_week: 20, maximum_valid_week: 30)
-  # Bebe nascido da Eri
+  # Bebe da Eri
   Message.create!(text: 'Mensagem para bebê nascido do sexo feminino de 1 a 2 anos', gender: 'female', baby_target_type: 'born', category: prevention_category, minimum_valid_week: 52, maximum_valid_week: 104)
   # Nenhum bebê
   Message.create!(text: 'Mensagem para bebê nascido do sexo feminino com menos de 1 anos', gender: 'female', baby_target_type: 'born', category: prevention_category, maximum_valid_week: 52)
-  # Bebe da francisca e bebe nascido da Eri
+  # Bebe da Francisca e bebe da Eri
   Message.create!(text: 'Mensagem para bebê nascido independente do sexo com mais de 6 meses', gender: 'both', baby_target_type: 'born', category: prevention_category, minimum_valid_week: 24)
 
 end
