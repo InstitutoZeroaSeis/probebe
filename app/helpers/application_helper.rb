78973002dd6c06d3dcfce261c93633d5e51f0ddf
cartolari <bruno.cartolari@gmail.com>
2014-11-03 00:31:48 -0200

    Include pregnancy start date in profile edit form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  @@date_helper = Class.new do
+    include ActionView::Helpers::DateHelper
+  end.new
+
   def l(value, *args)
     I18n.localize(value, *args) if value
   end
@@ -21,5 +25,12 @@ module ApplicationHelper
     form_builder.hidden_field(:_destroy, value: false) +
       link_to(name, '#', class: 'remove_fields ' + css_class, data: { container_to_hide: container_to_hide })
   end
+
+  def distance_of_time_in_words_to_now(time, *args)
+    if time
+      @@date_helper.distance_of_time_in_words_to_now(time, *args)
+    end
+  end
+  alias_method :time_ago_in_words, :distance_of_time_in_words_to_now
 
 end
