2de1906bfb9a1cea374891d8f25bbcacd85939fc
cartolari <bruno.cartolari@gmail.com>
2014-11-04 15:40:41 -0200

    Split oauth sign in and sign up specs

--- spec/features/user_sign_up_through_omniauth_spec.rb
+++ spec/features/user_sign_up_through_omniauth_spec.rb
@@ -3,45 +3,21 @@ require 'rails_helper'
 feature "OAuth2 authentication" do
   before { OmniAuth.config.test_mode = true }
 
-  context "With a new user" do
-    context "With valid credentials" do
-      before { OmniAuth.config.add_mock :google_oauth2, OmniAuthStub::Google::BasicInfo }
-      scenario "User sign up through omniauth" do
-        sign_in_through_oauth
-        expect(current_path).to eq(edit_profile_path)
-      end
-    end
-
-    context "With invalid credentials" do
-      before { OmniAuth.config.add_mock :google_oauth2, OmniAuthStub::Google::WithoutEmail }
-      scenario "User gets redirected back to login page", js: true do
-        sign_in_through_oauth
-        expect(page).to have_text(I18n.t('controller.messages.could_not_sign_up_with_omniauth'))
-        expect(current_path).to eq(new_user_session_path)
-      end
-    end
-  end
-
-  context "An existing user" do
+  context "With valid credentials" do
     before { OmniAuth.config.add_mock :google_oauth2, OmniAuthStub::Google::BasicInfo }
-    before { @user = create(:user, email: OmniAuthStub::Google::BasicInfo[:info][:email]) }
-
-    context "With a complete profile" do
-      before { create(:profile, user: @user) }
-      scenario "Gets redirected to its profile page" do
-        sign_in_through_oauth
-        expect(current_path).to eq(root_path)
-      end
-    end
-
-    context "With an incomplete profile" do
-      before { create(:profile, :without_children, :without_cell_phone, user: @user) }
-      context "With a incomplete profile" do
-        scenario "Gets redirected to its profile edit page" do
-          sign_in_through_oauth
-          expect(current_path).to eq(edit_profile_path)
-        end
-      end
+    scenario "User sign up through omniauth" do
+      sign_in_through_oauth
+      expect(current_path).to eq(edit_profile_path)
     end
   end
+
+  context "With invalid credentials" do
+    before { OmniAuth.config.add_mock :google_oauth2, OmniAuthStub::Google::WithoutEmail }
+    scenario "User gets redirected back to login page", js: true do
+      sign_in_through_oauth
+      expect(page).to have_text(I18n.t('controller.messages.could_not_sign_up_with_omniauth'))
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+
 end
