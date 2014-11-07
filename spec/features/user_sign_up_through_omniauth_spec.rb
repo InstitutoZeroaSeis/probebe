f55c9cc074ae60b62a99ae453b273d8627379518
cartolari <bruno.cartolari@gmail.com>
2014-11-07 14:37:14 -0200

    Change spec description

--- spec/features/user_sign_up_through_omniauth_spec.rb
+++ spec/features/user_sign_up_through_omniauth_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 feature "OAuth2 authentication" do
   before { OmniAuth.config.test_mode = true }
 
-  context "With valid credentials" do
+  context "with valid credentials" do
     before { OmniAuth.config.add_mock :google_oauth2, OmniAuthStub::Google::BasicInfo }
-    scenario "User sign up through omniauth" do
+    scenario "user gets redirected to its profile edit page" do
       sign_in_through_oauth
       expect(current_path).to eq(edit_profile_path)
     end
   end
 
-  context "With invalid credentials" do
+  context "with invalid credentials" do
     before { OmniAuth.config.add_mock :google_oauth2, OmniAuthStub::Google::WithoutEmail }
-    scenario "User gets redirected back to login page", js: true do
+    scenario "user gets redirected back to login page", js: true do
       sign_in_through_oauth
       expect(page).to have_text(I18n.t('controller.messages.could_not_sign_up_with_omniauth'))
       expect(current_path).to eq(new_user_session_path)
