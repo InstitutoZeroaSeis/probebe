983170a85d5fc2f8addd4fa79ae9c94e2ffec2f8
cartolari <bruno.cartolari@gmail.com>
2014-11-11 15:15:01 -0200

    Refactor article factories to remove duplication

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "The #{factory_name} factory" do
     it 'is valid' do
-      expect(build(factory_name)).to be_valid
+      expect(build_stubbed(factory_name)).to be_valid
     end
   end
 end
