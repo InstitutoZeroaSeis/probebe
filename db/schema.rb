5835706163df9b86bc901e7379ab3dd61907eabc
cartolari <bruno.cartolari@gmail.com>
2014-11-12 15:31:15 -0200

    Schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(version: 20141106185222) do
     t.string   "title"
     t.text     "text"
     t.text     "summary"
-    t.integer  "original_author_id"
     t.integer  "user_id"
     t.integer  "category_id"
     t.integer  "parent_article_id"
+    t.integer  "original_author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
