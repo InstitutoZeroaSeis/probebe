d29e83f47b08eba83ae3f10733f1a377371c50df
cartolari <bruno.cartolari@gmail.com>
2014-11-13 20:22:33 -0200

    Blog pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
+  POSTS_PER_PAGE = 10
   layout "blog"
 
   def index
-    @posts = Articles::JournalisticArticle.all
+    build_pager
+    @posts = @pager.paged
   end
 
   def show
     @post = Articles::JournalisticArticle.find(params[:id])
+  end
+
+  private
+
+  def build_pager
+    page = params[:page] || 0
+    @pager = Pager.new(Articles::JournalisticArticle.order(created_at: :desc), page, POSTS_PER_PAGE)
   end
 end
--- spec/support/features/regex_helper.rb
+++ spec/support/features/regex_helper.rb
@@ -3,5 +3,9 @@ module Features
     def extract_link_from_text(link_class, text)
       /<a class="#{link_class}" href="(.*)">/.match(text).captures.first
     end
+
+    def strip_tags(text)
+      Nokogiri::HTML(text).text
+    end
   end
 end
