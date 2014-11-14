2efe94c6f3825c1579109e5dba06b90802b97034
Hideki <hideki@vizir.com.br>
2014-11-14 15:51:33 -0200

    Created two more test for message sender

--- spec/models/message_sender_spec.rb
+++ spec/models/message_sender_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe MessageSender, :type => :model do
 
   context "with an already sent message" do
-    before { @article = create(:authorial_article, maximum_valid_week: 14, baby_target_type: 'born', messages: create_list(:message, 1, text: "Text")) }
+    before { @article = create(:journalistic_article, maximum_valid_week: 14, baby_target_type: 'born', messages: create_list(:message, 1, text: "Text")) }
     before { @profile = create(:profile, children: create_list(:child, 1, birth_date: 3.months.ago)) }
     before { @finder = ProfileFinder.new(@article) }
     before { MessageDelivery.create!(message: @article.messages.first, profiles: [@profile])}
@@ -14,5 +14,29 @@ RSpec.describe MessageSender, :type => :model do
     end
   end
 
+  context "with two message from different articles" do
+    before { @article = create(:journalistic_article, maximum_valid_week: 14, baby_target_type: 'born', messages: create_list(:message, 1, text: "Text")) }
+    before { @article2 = create(:journalistic_article, minimum_valid_week: 12 , baby_target_type: 'born', messages: create_list(:message, 1, text: "Text")) }
+    before { @profile = create(:profile, children: create_list(:child, 1, birth_date: 3.months.ago)) }
+    before { @finder = ProfileFinder.new(@article) }
+    before { @finder2 = ProfileFinder.new(@article2) }
+    it "Expect to have two message again" do
+      MessageSender.new(@finder).send_messages
+      MessageSender.new(@finder2).send_messages
+      expect(@profile.message_deliveries.count).to eq(2)
+    end
+  end
+
+  context "with message of same article" do
+    before { @article = create(:authorial_article, maximum_valid_week: 14, baby_target_type: 'born', messages: create_list(:message, 2, text: "Text")) }
+    before { @profile = create(:profile, children: create_list(:child, 1, birth_date: 3.months.ago)) }
+    before { @finder = ProfileFinder.new(@article) }
+    before { MessageDelivery.create!(message: @article.messages.first, profiles: [@profile])}
+    it "expect to send two message" do
+      expect(@profile.message_deliveries.count).to eq(1)
+      MessageSender.new(@finder).send_messages
+      expect(@profile.message_deliveries.count).to eq(2)
+    end
+  end
 
 end
