5c45e55aaa03af437214b98a1fc46c3554c8ff78
Hideki <hideki@vizir.com.br>
2014-11-26 14:09:26 -0200

    Fix script import profile

--- script/import_profile_csv.rb
+++ script/import_profile_csv.rb
@@ -33,5 +33,13 @@ CSV.foreach('data/PlanilhaMae.csv', headers: true, col_sep: ";" ) do |row|
       line_with_problem << (line_of_profile.to_s + row.to_s)
     end
   end
-  puts line_with_problem
+end
+cell_phone_from_probebe_employee = [11971000096,11999963951,11983636060,21993238857,11969239451]
+cell_phone_from_probebe_employee.each do |c|
+  email = c.to_s + "@funcionario_probebe.com.br"
+  user = User.create!(email: email, role: 'site_user')
+  user.confirm!
+
+  profile = Profile.create!(first_name: "funcionario", last_name: "probebe", user: user )
+  profile.cell_phones << CellPhone.create!(number: c)
 end
