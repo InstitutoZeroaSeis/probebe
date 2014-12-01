cffc50871254f894d3805d8e31f2593fe6f2e094
Hideki <hideki@vizir.com.br>
2014-12-01 18:13:29 -0200

    Created test for message sender

--- spec/models/message_sender_spec.rb
+++ spec/models/message_sender_spec.rb
@@ -1,6 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe MessageSender, :type => :model do
+  context "with many messages send message randomly" do
+    subject{ MessageSender.new(@message_finder).send_messages.message }
+    before { @profile = create(:profile, children: create_list(:child, 1, birth_date: 5.months.ago)) }
+    before { @article1 = create(:journalistic_article, :male, maximum_valid_week: 22, baby_target_type: 'born', messages: create_list(:message, 10)) }
+    before { @article2 = create(:journalistic_article, :both, minimum_valid_week: 10, baby_target_type: 'born', messages: create_list(:message, 10)) }
+    before { @article_finder = ArticlesFinder.new(Articles::JournalisticArticle.all, @profile.children.first) }
+    before { @message_finder = MessageFinder.new(@article_finder) }
+    before { @message_sender = MessageSender.new(@message_finder).send_messages }
+    it { is_expected.not_to eq(@message_sender.message) }
+  end
+
+  context "with only one message and already sent before" do
+    subject{ MessageSender.new(@message_finder).send_messages }
+    before { @profile = create(:profile, children: create_list(:child, 1, birth_date: 5.months.ago)) }
+    before { @article1 = create(:journalistic_article, :male, maximum_valid_week: 22, baby_target_type: 'born', messages: create_list(:message, 1)) }
+    before { @message1 = @article1.messages.first}
+    before { MessageDelivery.create!(profile: @profile, message: @message1) }
+    before { @article_finder = ArticlesFinder.new(Articles::JournalisticArticle.all, @profile.children.first) }
+    before { @message_finder = MessageFinder.new(@article_finder) }
+    it { is_expected.to be_nil }
+  end
+
+  context "with no message for that profile" do
+    subject{ MessageSender.new(@message_finder).send_messages }
+    before { @profile = create(:profile, children: create_list(:child, 1, birth_date: 5.months.ago)) }
+    before { @article1 = create(:journalistic_article, :male, maximum_valid_week: 22, baby_target_type: 'pregnancy', messages: create_list(:message, 12)) }
+    before { @article_finder = ArticlesFinder.new(Articles::JournalisticArticle.all, @profile.children.first) }
+    before { @message_finder = MessageFinder.new(@article_finder) }
+    it { is_expected.to be_nil }
+  end
 
 
 end
