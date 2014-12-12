458635cc265aa006fd580b924422766e538659be
Hideki <hideki@vizir.com.br>
2014-12-12 11:49:09 -0200

    Fix valid format regex for cell phone

--- app/models/cell_phone.rb
+++ app/models/cell_phone.rb
@@ -1,7 +1,7 @@
 class CellPhone < ActiveRecord::Base
   belongs_to :profile
   validates_presence_of :number, :area_code
-  validates_format_of :number, with: /\d{4,5}\-\d{4,4}/
-  validates_format_of :area_code, with: /\d\d/
+  validates_format_of :number, with: /\A\d{4,5}\-\d{4,4}\Z/
+  validates_format_of :area_code, with: /\A\d\d\Z/
 
 end
