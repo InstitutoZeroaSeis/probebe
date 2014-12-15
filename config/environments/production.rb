eb94ce65ffb9e7033a8fdb1c70cea2474098e680
Hideki <hideki@vizir.com.br>
2014-12-15 16:22:58 -0200

    Changed deliver_sms at production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.routes.default_url_options[:host] = 'probebe.vzr.com.br'
+  Rails.application.routes.default_url_options[:host] = 'www.probebe.org.br'
 
-  config.message_delivery.deliver_sms = true
+  config.message_delivery.deliver_sms = false
 end
