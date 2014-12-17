fbf7027c8df706f4106b96f1dc5d072593991680
cartolari <bruno.cartolari@gmail.com>
2014-12-17 16:29:58 -0200

    Fix user vists a specific post spec bug

--- spec/features/user_visits_a_specific_post_spec.rb
+++ spec/features/user_visits_a_specific_post_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 feature "User visits a specific post" do
   scenario "successfully" do
-    posts = create_list(:journalistic_article, 5)
-    first_post = posts.first
+    post = create(:journalistic_article)
+    create_list(:journalistic_article, 5, created_at: 2.days.ago)
     visit root_path
-    click_on posts.first.title
-    expect(current_path).to eq(post_path(first_post))
-    expect(page).to have_content(first_post.title)
+    click_on post.title
+    expect(current_path).to eq(post_path(post))
+    expect(page).to have_content(post.title)
   end
 end
