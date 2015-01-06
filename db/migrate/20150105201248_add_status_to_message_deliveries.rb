b0260e17131d0e647b87e4fd835690bd1c06d590
cartolari <bruno.cartolari@gmail.com>
2015-01-06 14:24:47 -0200

    Change add status to message deliveries migration to fix the status default value

--- db/migrate/20150105201248_add_status_to_message_deliveries.rb
+++ db/migrate/20150105201248_add_status_to_message_deliveries.rb
@@ -1,5 +1,12 @@
 class AddStatusToMessageDeliveries < ActiveRecord::Migration
+  class MessageDelivery < ActiveRecord::Base
+  end
+
   def change
     add_column :message_deliveries, :status, :integer, default: 0
+    MessageDelivery.reset_column_information
+    reversible do |dir|
+      dir.up { MessageDelivery.update_all status: 1 }
+    end
   end
 end
