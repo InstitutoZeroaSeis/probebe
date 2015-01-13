019314091db976e62e7e04c48c33a0001aaa3f23
Hideki <hideki@vizir.com.br>
2015-01-13 15:44:07 -0200

    Created profile presenter

--- app/presenters/admin/profile_presenter.rb
+++ app/presenters/admin/profile_presenter.rb
@@ -1,3 +1,50 @@
 class Admin::ProfilePresenter < Carnival::BaseAdminPresenter
+
   field :id
+
+  field :user_id,
+    as: :hidden,
+    actions: [:new]
+
+  field :name,
+    actions: [:index, :show]
+
+  field :first_name,
+    actions: [:new, :edit],
+    sortable: true,
+    advanced_search: {operator: :equal}
+
+  field :last_name,
+    actions: [:new, :edit],
+    sortable: true,
+    advanced_search: {operator: :equal}
+
+  field :birth_date,
+    actions: [:index, :show, :new, :edit]
+
+  field :state,
+    actions: [:index, :show, :new, :edit]
+
+  field :city,
+    actions: [:index, :show, :new, :edit]
+
+  field :street,
+    actions: [:index, :show, :new, :edit]
+
+  field :home_phone_number,
+    actions: [:index, :show, :new, :edit]
+
+  field 'primary_cell_phone_number',
+    actions: [:index, :show]
+
+  field :cell_phones,
+    nested_form: true,
+    nested_form_allow_destroy: true,
+    nested_form_modes: [:new, :edit]
+
+  field :avatars,
+    nested_form: true,
+    nested_form_allow_destroy: true,
+    nested_form_modes: [:new, :edit]
+
 end
