205941aa496ff43612bbbd5ffa4290a93af2faea
cartolari <bruno.cartolari@gmail.com>
2015-01-14 09:49:22 -0200

    Change the find_and_deliver_messages to be a class method

--- app/models/message_deliveries/message_delivery_finder.rb
+++ app/models/message_deliveries/message_delivery_finder.rb
@@ -1,6 +1,6 @@
 module MessageDeliveries
   class MessageDeliveryFinder
-    def find_and_deliver_messages
+    def self.find_and_deliver_messages
       MessageDelivery.created_today.not_sent.each do |message_delivery|
         sender = MessageSender.new(message_delivery)
         sent = sender.send_to_device
