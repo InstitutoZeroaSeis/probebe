4be6fd2dc08e7ee5b62fdd85bf00a9dad7d85ad2
Hideki <hideki@vizir.com.br>
2015-01-29 15:39:21 -0200

    Created spec for category matcher

--- spec/factories/category_factory.rb
+++ spec/factories/category_factory.rb
@@ -4,4 +4,23 @@ FactoryGirl.define do
     parent_category 'health'
   end
 
+  trait :with_health_parent do
+    parent_category 'health'
+  end
+
+  trait :with_education_parent do
+    parent_category 'education'
+  end
+
+  trait :with_security_parent do
+    parent_category 'security'
+  end
+
+  trait :with_finance_parent do
+    parent_category 'finance'
+  end
+
+  trait :with_socio_emotional_parent do
+    parent_category 'socio_emotional'
+  end
 end
