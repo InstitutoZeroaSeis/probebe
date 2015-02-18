f105371d2591568b38801aa9e87b4399b5698e3c
cartolari <bruno.cartolari@gmail.com>
2015-02-18 14:27:22 -0200

    Update cron accordingly to N. Virgina timezone

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,8 @@
-every [:monday , :wednesday, :friday],  :at => "11am" do
+# This app runs in a server located in N. Virginia,
+# and theere is a difference of two hours between, Sao Paulo
+# and N. Virginia, so we setup whenever to run at 1 pm N. Virgina
+# the tasks will be run at 11 am in Sao Paulo
+every [:monday, :wednesday, :friday],  at: '1pm' do
   command 'curl -X POST http://nginx/api/d3d4b74ea38c163c820cd84b25f5/a8eecbf2d604ff6769fd64f1a491'
   command 'echo "Running cron at $(date)" >> /app/log/cron.log '
 end
