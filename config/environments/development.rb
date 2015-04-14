29020ad32832393b9cc8bfbf8b8efb7a6bd47cbd
cartolari <bruno.cartolari@gmail.com>
2015-04-13 21:24:55 -0300

    Initial implementation of the new profile page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  BetterErrors::Middleware.allow_ip! "0.0.0.0/0" if defined? BetterErrors && Rails.env == :development
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0" if defined? BetterErrors
 
   config.after_initialize do
     Bullet.enable = true
