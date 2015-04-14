b14b6d462262a1fc7d85ee47b0b0daaae1fd82a6
cartolari <bruno.cartolari@gmail.com>
2015-04-14 14:45:50 -0300

    Refactor rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,10 +8,13 @@ require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter 'vendor/'
   add_filter 'spec/matchers/cancan_custom_matchers.rb'
-  add_group 'Presenters', 'app/presenters'
-  add_group 'Forms', 'app/forms/'
+
   add_group 'Inputs', 'app/inputs/'
+  add_group 'Presenters', 'app/presenters'
+  add_group 'Value Objects', 'app/value_objects/'
   add_group 'View Objects', 'app/view_objects/'
+  add_group 'Workers', 'app/workers/'
+  add_group 'Serializers', 'app/serializers/'
 end
 # Requires all files with ActiveSupport require_dependency, so we can
 # have 100% coverage and the Rails autoload feature will still works
@@ -20,7 +23,7 @@ Dir[Rails.root.join('lib/**/*.rb')].each { |f| require_dependency f }
 
 Capybara.javascript_driver = :webkit
 
-Dir.glob(Rails.root.join('spec/factories/**/*.rb')).each {|factory| require factory }
+Dir[Rails.root.join('spec/factories/**/*.rb')].each {|factory| require factory }
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 Dir[Rails.root.join("spec/stubs/**/*.rb")].each { |f| require f }
 
