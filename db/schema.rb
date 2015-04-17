c1ff52661dfcd4c7678ad4310a17b989cd2ed081
Hideki <hideki@vizir.com.br>
2015-04-17 17:18:26 -0300

    Added show author to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127160904) do
+ActiveRecord::Schema.define(version: 20150417195940) do
 
   create_table "article_references", force: true do |t|
     t.string   "source"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150127160904) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "publishable",                 default: false
+    t.boolean  "show_author",                 default: true
   end
 
   create_table "articles_tags", id: false, force: true do |t|
