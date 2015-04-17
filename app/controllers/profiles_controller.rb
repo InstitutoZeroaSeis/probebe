964fed9544282fd1fca4c0f83f84a67ca0628394
cartolari <bruno.cartolari@gmail.com>
2015-04-17 14:51:18 -0300

    Improve structure of profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,19 +2,17 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :deny_admin_site_user_access_on_public_site
   before_action :check_profile_status, except: [:new, :create, :edit, :update]
+  before_action :instantiate_profile
   layout 'blog'
 
   def show
-    @profile = current_profile
   end
 
   def new
-    @profile = Profile.new
     redirect_to edit_profile_path if current_profile
   end
 
   def create
-    @profile = Profile.new
     @profile.user = current_user
     @profile.attributes = permitted_params
     if @profile.save
@@ -25,14 +23,12 @@ class ProfilesController < ApplicationController
   end
 
   def edit
-    @profile = current_profile
     if current_profile.blank?
       redirect_to new_profile_path
     end
   end
 
   def update
-    @profile = current_profile
     if @profile.update_attributes(permitted_params)
       redirect_to profile_path
     else
@@ -42,6 +38,12 @@ class ProfilesController < ApplicationController
 
   def save_profile
     redirect_to profile_path if @profile.save
+  end
+
+  protected
+
+  def instantiate_profile
+    @profile = ProfilePresenter.new(current_profile || Profile.new)
   end
 
   def permitted_params
