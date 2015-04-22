d51061ca5f72bb64f7739ae809364e2bb5ff0dea
cartolari <bruno.cartolari@gmail.com>
2015-04-21 22:33:32 -0300

    Remove rules to completely fill the profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,5 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
-  before_action :deny_admin_site_user_access_on_public_site
-  before_action :check_profile_status, except: [:new, :create, :edit, :update]
-  before_action :instantiate_profile
   layout 'blog'
 
   def show
