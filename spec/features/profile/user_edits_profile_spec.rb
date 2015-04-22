8e97026bc766ad1149f3885480ecfba0ee63c9fa
cartolari <bruno.cartolari@gmail.com>
2015-04-22 16:10:50 -0300

    Add profile spec

---
+++ spec/features/profile/user_edits_profile_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'User edits new profile' do
+  let(:user) { create(:user, :confirmed, :with_profile) }
+  before { sign_in(user.email, user.password) }
+
+  scenario 'successfully' do
+    visit edit_profile_path
+    fill_in 'profile_first_name', with: 'New name'
+
+    click_button 'Atualizar Perfil'
+
+    expect(page).to have_content('New name')
+  end
+
+  scenario 'with invalid data' do
+    visit edit_profile_path
+    fill_in 'profile_first_name', with: ''
+    click_button 'Atualizar Perfil'
+
+    expect(page).to have_content(I18n.t('simple_form.error_notification.default_message'))
+  end
+end
