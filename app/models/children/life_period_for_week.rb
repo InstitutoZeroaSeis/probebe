96d537a794ce41feffcd6ac1238350069eb30cde
cartolari <bruno.cartolari@gmail.com>
2015-04-23 17:48:41 -0300

    Improve LifePeriodForWeek algorithm

--- app/models/children/life_period_for_week.rb
+++ app/models/children/life_period_for_week.rb
@@ -1,13 +1,15 @@
 module Children
   class LifePeriodForWeek
-    PERIOD_FOR_FIRST_TO_FOURTH_MONTH = 0..16
-    PERIOD_FOR_FIFTH_TO_EIGHTH_MONTH = 17..32
-    PERIOD_FOR_NINETH_TO_TWELFTH_MONTH = 33..48
-    PERIOD_FOR_THIRTEENTH_TO_FIFTEENTH_MONTH = 49..60
+    RANGES = [
+      PERIOD_FOR_FIRST_TO_FOURTH_MONTH = 0..16,
+      PERIOD_FOR_FIFTH_TO_EIGHTH_MONTH = 17..32,
+      PERIOD_FOR_NINETH_TO_TWELFTH_MONTH = 33..48,
+      PERIOD_FOR_THIRTEENTH_TO_FIFTEENTH_MONTH = 49..60
+    ]
 
-    def initialize(min_week, max_week, pregnancy)
-      @min_week = min_week
-      @max_week = max_week
+    def initialize(start_week, end_week, pregnancy)
+      @start_week = start_week
+      @end_week = end_week
       @pregnancy = pregnancy
     end
 
@@ -22,7 +24,7 @@ module Children
     protected
 
     def born_period
-      case @min_week || @max_week
+      case to_compare
       when PERIOD_FOR_FIRST_TO_FOURTH_MONTH
         :first_to_fourth
       when PERIOD_FOR_FIFTH_TO_EIGHTH_MONTH
@@ -34,6 +36,32 @@ module Children
       else
         :thirteenth_to_fifteenth
       end
+    end
+
+    def to_compare
+      if [@start_week, @end_week].include? nil
+        @start_week || @end_week
+      else
+        smallest_distance.last
+      end
+    end
+
+    def smallest_distance
+      distance.sort_by(&:first).first
+    end
+
+    def distance
+      RANGES.map do |range|
+        smallest_from_start = min_distance_from_start_or_end(range.first)
+        smallest_from_end = min_distance_from_start_or_end(range.last)
+        smallest_distance = [smallest_from_start, smallest_from_end].min
+
+        [smallest_distance, range]
+      end
+    end
+
+    def min_distance_from_start_or_end(value)
+      [(value - @start_week).abs, (value - @end_week).abs].min
     end
   end
 end
