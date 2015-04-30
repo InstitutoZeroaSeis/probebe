6ba33aef0ca2b7a91b3a16746c5d4c4c48677ab3
cartolari <bruno.cartolari@gmail.com>
2015-04-29 21:06:15 -0300

    Add a link from messages to article

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,11 +1,14 @@
 class Admin::MessagesController < Admin::AdminController
   load_and_authorize_resource class: 'Message'
-  defaults :resource_class => Message
+  defaults resource_class: Message
 
-  private
+  protected
+
+  def table_items
+    Message.includes(:messageable)
+  end
 
   def permitted_params
     params.permit(message: [:text])
   end
-
 end
--- app/presenters/admin/message_presenter.rb
+++ app/presenters/admin/message_presenter.rb
@@ -1,23 +1,20 @@
-
 class Admin::MessagePresenter < Carnival::BaseAdminPresenter
-
   field :id,
-    actions: [:index, :show], :sortable => false,
-    advanced_search: {:operator => :equal}
+        actions: [:index, :show], sortable: false,
+        advanced_search: { operator: :equal }
 
   field :text,
-    actions: [:index, :new, :edit, :show],
-    advanced_search: {:operator => :like},
-    as: :char_countable
+        actions: [:index, :new, :edit, :show],
+        advanced_search: { operator: :like },
+        as: :char_countable
 
-  field :messageable_id,
-    actions: [:index, :show]
+  field 'messageable.name',
+        actions: [:index, :show],
+        as: :partial
 
-  field :created_at, :actions => [:index, :show]
+  field :created_at, actions: [:index, :show]
 
   action :show
   action :edit
   action :destroy
-  action :new
-
 end
