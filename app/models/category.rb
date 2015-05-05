39dc496c3cae7cc71e4e1a61e728697065d40a5b
cartolari <bruno.cartolari@gmail.com>
2015-05-05 15:52:22 -0300

    Cycle validations in category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,5 @@
 class Category < ActiveRecord::Base
   include Carnival::ModelHelper
-  [
-    :health, :education, :security, :finance, :socio_emotional
-  ]
 
   belongs_to :parent_category, class_name: 'Category'
   has_many :articles, class_name: 'Articles::Article'
@@ -10,6 +7,26 @@ class Category < ActiveRecord::Base
   has_many :messages
 
   validates :name, presence: true
+  validate :parent_category_is_not_equals_self
+  validate :parent_category_is_at_most_two_levels_deep
+  validate :parent_category_cannot_be_a_children
 
   has_paper_trail
+
+  protected
+
+  def parent_category_is_not_equals_self
+    return unless parent_category == self
+    errors.add(:parent_category, 'não pode ser igual a categoria')
+  end
+
+  def parent_category_is_at_most_two_levels_deep
+    return unless parent_category.try(:parent_category)
+    errors.add(:parent_category, 'não pode ter mais que dois níveis')
+  end
+
+  def parent_category_cannot_be_a_children
+    return unless parent_category.try(:parent_category) == self
+    errors.add(:parent_category, 'não pode ser igual a uma subcategoria')
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -16,4 +16,64 @@ describe Category do
 
     expect(category.errors[:name]).to include('não pode ser vazio')
   end
+
+  it 'is valid with another parent category' do
+    category = Category.new
+    category.parent_category = Category.new
+
+    category.valid?
+
+    expect(category.errors[:parent_category])
+      .to_not include('não pode ser igual a categoria')
+  end
+
+  it 'is invalid with parent category equals to self' do
+    category = Category.new
+    category.parent_category = category
+
+    category.valid?
+
+    expect(category.errors[:parent_category])
+      .to include('não pode ser igual a categoria')
+  end
+
+  it 'is valid with at most two leves of category depth' do
+    category = Category.new(parent_category: Category.new)
+
+    category.valid?
+
+    expect(category.errors[:parent_category])
+      .to_not include('não pode ter mais que dois níveis')
+  end
+
+  it 'is invalid with more than two leves of category depth' do
+    category_with_parent = Category.new(parent_category: Category.new)
+    category = Category.new(parent_category: category_with_parent)
+
+    category.valid?
+
+    expect(category.errors[:parent_category])
+      .to include('não pode ter mais que dois níveis')
+  end
+
+  it 'is valid with no cycles in the parent_category' do
+    parent_category = Category.new
+    child_category = Category.new(parent_category: parent_category)
+
+    parent_category.valid?
+
+    expect(parent_category.errors[:parent_category])
+      .to_not include('não pode ser igual a uma subcategoria')
+  end
+
+  it 'is invalid with a parent_category a cycle in the parent_category' do
+    parent_category = Category.new
+    child_category = Category.new(parent_category: parent_category)
+    parent_category.parent_category = child_category
+
+    parent_category.valid?
+
+    expect(parent_category.errors[:parent_category])
+      .to include('não pode ser igual a uma subcategoria')
+  end
 end
