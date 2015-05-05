42d68af9c6ddd14e3aea08579084db62813e7df0
cartolari <bruno.cartolari@gmail.com>
2015-05-05 15:54:32 -0300

    Fix category factory

--- spec/factories/category_factory.rb
+++ spec/factories/category_factory.rb
@@ -1,26 +1,5 @@
 FactoryGirl.define do
   factory :category do
-    sequence(:name) {|n| "#{n}category"}
-    parent_category 'health'
-  end
-
-  trait :with_health_parent do
-    parent_category 'health'
-  end
-
-  trait :with_education_parent do
-    parent_category 'education'
-  end
-
-  trait :with_security_parent do
-    parent_category 'security'
-  end
-
-  trait :with_finance_parent do
-    parent_category 'finance'
-  end
-
-  trait :with_socio_emotional_parent do
-    parent_category 'socio_emotional'
+    sequence(:name) { |n| "Category #{n}" }
   end
 end
