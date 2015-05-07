1a9e527321a7f95cf3d5e94baa0a75c6754ce250
cartolari <bruno.cartolari@gmail.com>
2015-05-07 03:41:53 -0300

    Log exceptions on errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,7 @@
 class ErrorsController < ApplicationController
   def show
-    @exception = env["action_dispatch.exception"]
+    @exception = env['action_dispatch.exception']
+    logger.info @exception.inspect
     render action: request.path[1..-1]
   end
 end
