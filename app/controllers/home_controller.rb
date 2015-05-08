f56d567bcde8a73f0436a797a194410412b80bed
cartolari <bruno.cartolari@gmail.com>
2015-05-08 01:39:59 -0300

    Use PostPresenter in _signed_in view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
   def index
     return unless current_user
-    @categories_articles =
-      CategorySampleArticlesDecorator.group_by_parent_category
+    @categories_posts = posts_grouped_by_category.map do |category, posts|
+      [category, PostPresenter.wrap(posts)]
+    end
+  end
+
+  protected
+
+  def posts_grouped_by_category
+    CategorySampleArticlesDecorator.group_by_parent_category
   end
 end
