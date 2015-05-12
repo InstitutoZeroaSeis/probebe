81683ea5d52760bd3572855cc1063f3615305a9e
cartolari <bruno.cartolari@gmail.com>
2015-05-12 19:41:36 -0300

    Add created at to articles

--- app/presenters/admin/journalistic_article_presenter.rb
+++ app/presenters/admin/journalistic_article_presenter.rb
@@ -1,90 +1,91 @@
 class Admin::JournalisticArticlePresenter < Carnival::BaseAdminPresenter
-
   model_name 'Articles::JournalisticArticle'
 
   field :parent_article_id,
-    as: :hidden,
-    actions: [:index, :show, :new],
-    sortable: true,
-    advanced_search: {operator: :equal}
+        as: :hidden,
+        actions: [:index, :show, :new],
+        sortable: true,
+        advanced_search: { operator: :equal }
 
   field :original_author_id,
-    as: :admin_site_user_collection,
-    actions: [:new, :edit],
-    advanced_search: {operator: :like}
+        as: :admin_site_user_collection,
+        actions: [:new, :edit],
+        advanced_search: { operator: :like }
 
   field 'category.name',
-    actions: [:show, :index],
-    advanced_search: {operator: :equal}
+        actions: [:show, :index],
+        advanced_search: { operator: :equal }
 
   field :category,
         actions: [:new, :edit]
 
   field :tags,
-    actions: [:new, :show, :edit],
-    nested_form: true,
-    nested_form_modes: [:associate],
-    show_as_list: true
+        actions: [:new, :show, :edit],
+        nested_form: true,
+        nested_form_modes: [:associate],
+        show_as_list: true
 
   field :title,
-    actions: [:index, :show, :edit, :new],
-    sortable: true,
-    advanced_search: {operator: :like}
+        actions: [:index, :show, :edit, :new],
+        sortable: true,
+        advanced_search: { operator: :like }
 
   field :text,
-    as: :ckeditor,
-    actions: [:show, :edit, :new],
-    sortable: true
+        as: :ckeditor,
+        actions: [:show, :edit, :new],
+        sortable: true
 
   field :image_cover,
-    as: :partial,
-    actions: [:edit, :new]
+        as: :partial,
+        actions: [:edit, :new]
 
   field :summary,
-    actions: [:index, :show, :edit, :new],
-    sortable: true,
-    advanced_search: {operator: :like}
+        actions: [:index, :show, :edit, :new],
+        sortable: true,
+        advanced_search: { operator: :like }
 
   field :box,
-    actions: [:index, :show, :edit, :new],
-    sortable: true,
-    advanced_search: {operator: :like}
+        actions: [:index, :show, :edit, :new],
+        sortable: true,
+        advanced_search: { operator: :like }
 
   field :gender,
-    actions: [:index, :new, :edit, :show],
-    advanced_search: {:operator => :equal},
-    as: :enum
+        actions: [:index, :new, :edit, :show],
+        advanced_search: { operator: :equal },
+        as: :enum
 
   field :teenage_pregnancy,
-    actions: [:index, :new, :edit, :show],
-    advanced_search: {:operator => :equal}
+        actions: [:index, :new, :edit, :show],
+        advanced_search: { operator: :equal }
 
   field :baby_target_type,
-    actions: [:index, :new, :edit, :show],
-    advanced_search: {:operator => :equal},
-    as: :enum
+        actions: [:index, :new, :edit, :show],
+        advanced_search: { operator: :equal },
+        as: :enum
 
   field :minimum_valid_week,
-    actions: [:index, :new, :edit, :show],
-    advanced_search: {:operator => :equal}
+        actions: [:index, :new, :edit, :show],
+        advanced_search: { operator: :equal }
 
   field :maximum_valid_week,
-    actions: [:index, :new, :edit, :show],
-    advanced_search: {:operator => :equal}
+        actions: [:index, :new, :edit, :show],
+        advanced_search: { operator: :equal }
 
   field :publishable,
-    actions: [:index, :new, :edit, :show],
-    sortable: true
+        actions: [:index, :new, :edit, :show],
+        sortable: true
 
   field 'original_author_name',
-    actions: [:index, :show],
-    sortable: true,
-    advanced_search: {operator: :like}
+        actions: [:index, :show],
+        sortable: true,
+        advanced_search: { operator: :like }
 
   field :messages,
-    actions: [:new, :show, :edit],
-    nested_form: true,
-    nested_form_modes: [:new]
+        actions: [:new, :show, :edit],
+        nested_form: true,
+        nested_form_modes: [:new]
+
+  field :created_at, actions: [:index, :show]
 
   action :show
   action :edit
@@ -92,5 +93,4 @@ class Admin::JournalisticArticlePresenter < Carnival::BaseAdminPresenter
   action :show_activity_log
 
   index_as :list
-
 end
