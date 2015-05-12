adf53daa88d263bda6e498a7d44dee15e4b8b91b
cartolari <bruno.cartolari@gmail.com>
2015-05-12 16:22:28 -0300

    Fix and refactor journalistic_articles_controller

--- app/controllers/admin/journalistic_articles_controller.rb
+++ app/controllers/admin/journalistic_articles_controller.rb
@@ -2,29 +2,43 @@ class Admin::JournalisticArticlesController < Admin::AdminController
   defaults resource_class: Articles::JournalisticArticleWithCover
   load_and_authorize_resource class: 'Articles::JournalisticArticleWithCover'
 
-  layout "carnival/admin"
+  layout 'carnival/admin'
 
   def table_items
     Articles::JournalisticArticle.includes(:category, :user)
   end
 
   def build_resource
-    if action_name == 'new'
-      authorial_article = find_authorial_article(params[:id])
-      Articles::JournalisticArticleFactory.from_authorial_article authorial_article
-    elsif action_name == 'create'
-      authorial_article = find_authorial_article(params[:articles_journalistic_article][:parent_article_id])
-      Articles::JournalisticArticleFactory.from_authorial_article(authorial_article).tap do |article|
-        article.assign_attributes *build_resource_params
-      end
+    if needs_custom_article?
+      build_custom_article
     else
       super
     end
   end
 
-  private
+  protected
 
-  def find_authorial_article(id)
+  def build_custom_article
+    factory = Articles::JournalisticArticleFactory.new(find_authorial_article)
+    journalistic_article = factory.build
+    if action_name == 'create'
+      journalistic_article.assign_attributes(*build_resource_params)
+    end
+    journalistic_article
+  end
+
+  def needs_custom_article?
+    %w(new create).include? action_name
+  end
+
+  def find_authorial_article
+    return unless needs_custom_article?
+    id =
+      if action_name == 'new'
+        params[:id]
+      else
+        params[:articles_journalistic_article][:parent_article_id]
+      end
     Articles::AuthorialArticle.find(id)
   end
 
@@ -33,9 +47,9 @@ class Admin::JournalisticArticlesController < Admin::AdminController
       params.require(:articles_journalistic_article).permit(
         :id, :text, :title, :summary, :category_id, :user_id, :original_author_id,
         :gender, :teenage_pregnancy, :baby_target_type, :publishable, :image_cover,
-        :box, :minimum_valid_week, :maximum_valid_week, {tag_ids: []},
-        article_references_attributes:[:id, :source, :_destroy],
-        messages_attributes:[:id, :text, :_destroy]
+        :box, :minimum_valid_week, :maximum_valid_week, { tag_ids: [] },
+        article_references_attributes: [:id, :source, :_destroy],
+        messages_attributes: [:id, :text, :_destroy]
       ).merge(user_id: current_user.id)
     ]
   end
