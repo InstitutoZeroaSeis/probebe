fb6eae9259dc8a896d8c2cde7a9017cc345ff532
cartolari <bruno.cartolari@gmail.com>
2015-05-19 20:24:02 -0300

    Fix timeline post preview

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,18 +1,22 @@
 $(function() {
-  $('body').on('click', '.entry a', function(){
+  $('body').on('click', '.entry a', function(event){
+    event.preventDeafult()
+    var href = $(this).attr('href');
+    if (href.slice(-1) === '#') {
+      return;
+    }
   	$('.timeline-modal').empty();
   	$('.timeline-modal, .timeline-modal-close, .timeline-modal-bg').addClass('is-visible')
-  	$('.timeline-modal').load($(this).prop('href'), function() {
+  	$('.timeline-modal').load(href, function() {
   		$('.timeline-modal').addClass('remove-loading')
 		});
-    return false
   });
 
   $('body').on('click', '.timeline-modal-close, .timeline-modal-bg', function(){
   	$('.timeline-modal, .timeline-modal-close, .timeline-modal-bg').removeClass('is-visible')
   	$('.timeline-modal').removeClass('remove-loading')
   	$('.timeline-modal').empty();
-  	
+
     return false
   });
 
