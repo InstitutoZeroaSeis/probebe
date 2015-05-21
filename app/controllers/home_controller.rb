bbb83881006d831e590bc10efff5582bac553804
cartolari <bruno.cartolari@gmail.com>
2015-05-21 12:24:17 -0300

    Fix categories order for signed in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,23 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      @categories_posts = posts_grouped_by_category.map do |category, posts|
-        [category, PostPresenter.wrap(posts)]
-      end
+      @categories_posts = posts_grouped_by_category
     else
-      @categories = Category.original_categories.map do |category|
-        [category.original_category_type, category]
-      end.to_h
+      @categories = original_categories
     end
   end
 
   protected
 
+  def original_categories
+    Category.original_categories.map do |category|
+      [category.original_category_type, category]
+    end.to_h
+  end
+
   def posts_grouped_by_category
-    CategorySampleArticlesDecorator.group_by_parent_category
+    Blog::CategoryGroupedPostsFinder.new.find.map do |category, posts|
+      [category, PostPresenter.wrap(posts)]
+    end
   end
 end
