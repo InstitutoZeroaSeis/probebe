a5485a555ea85fa5662777aa2d11bbd19f084345
cartolari <bruno.cartolari@gmail.com>
2015-05-27 13:34:45 -0300

    Update forgery protection in MessageDeliveries

--- app/controllers/api/message_deliveries_controller.rb
+++ app/controllers/api/message_deliveries_controller.rb
@@ -1,6 +1,8 @@
 class Api::MessageDeliveriesController < ApplicationController
   include HeaderAuthenticationConcern
 
+  protect_from_forgery with: :null_session
+
   def create
     MessageSenderWorker.perform_async(Date.today)
     head :ok
