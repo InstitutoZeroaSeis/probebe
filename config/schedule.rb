e023f78b4abcb84b1d622da97ffe51bb4d6f6301
cartolari <bruno.cartolari@gmail.com>
2015-06-12 03:59:35 -0300

    Redirect cron output to log

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 every [:monday, :wednesday, :friday],  at: '11am' do
-  command 'curl -X POST http://www.probebe.org.br/api/d3d4b74ea38c163c820cd84b25f5/a8eecbf2d604ff6769fd64f1a491'
+  command 'curl -X POST http://www.probebe.org.br/api/d3d4b74ea38c163c820cd84b25f5/a8eecbf2d604ff6769fd64f1a491 >> /app/log/cron.log'
   command 'echo "Running cron at $(date)" >> /app/log/cron.log '
 end
