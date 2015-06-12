5be3fc22fb08f30d515a45fb88b926585c66d412
cartolari <bruno.cartolari@gmail.com>
2015-06-11 21:07:11 -0300

    Refactor message updater spec

--- spec/models/articles/message_updater_spec.rb
+++ spec/models/articles/message_updater_spec.rb
@@ -1,37 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe Articles::MessageUpdater, :type => :model do
-  context "given an article and a message" do
-    let(:article) { create :journalistic_article }
-    let(:message) { create :message }
+RSpec.describe Articles::MessageUpdater, type: :model do
+  it 'saves the message' do
+    attributes = {
+      gender: 'male', teenage_pregnancy: false,
+      baby_target_type: 'born', category: '',
+      minimum_valid_week: 20, maximum_valid_week: 30
+    }
+    article = OpenStruct.new(attributes)
+    message = spy('message')
 
-    it "is expected to update message attributes from the article" do
-      gender = 'male'
-      teenage_pregnancy = false
-      baby_target_type = 'born'
-      minimum_valid_week = 20
-      maximum_valid_week = 30
-      category = build_stubbed(:category)
+    Articles::MessageUpdater.update_from_article(message, article)
 
-      article = double('article')
-      allow(article).to receive(:gender).and_return(gender)
-      allow(article).to receive(:teenage_pregnancy).and_return(teenage_pregnancy)
-      allow(article).to receive(:baby_target_type).and_return(baby_target_type)
-      allow(article).to receive(:minimum_valid_week).and_return(minimum_valid_week)
-      allow(article).to receive(:maximum_valid_week).and_return(maximum_valid_week)
-      allow(article).to receive(:category).and_return(category)
+    expect(message).to have_received(:save!)
+  end
 
-      message = double('message')
-      expect(message).to receive(:gender=).with(gender)
-      expect(message).to receive(:teenage_pregnancy=).with(teenage_pregnancy)
-      expect(message).to receive(:baby_target_type=).with(baby_target_type)
-      expect(message).to receive(:minimum_valid_week=).with(minimum_valid_week)
-      expect(message).to receive(:maximum_valid_week=).with(maximum_valid_week)
-      expect(message).to receive(:category=).with(category)
-      allow(message).to receive(:save!)
+  it 'updates message attributes through articles' do
+    attributes = {
+      gender: 'male', teenage_pregnancy: false,
+      baby_target_type: 'born', category: '',
+      minimum_valid_week: 20, maximum_valid_week: 30
+    }
+    article = OpenStruct.new(attributes)
+    message = spy('message')
 
-      Articles::MessageUpdater.update_from_article(message, article)
+    Articles::MessageUpdater.update_from_article(message, article)
+
+    attributes.each do |key, value|
+      expect(message).to have_received("#{key}=").with(value)
     end
-
   end
 end
