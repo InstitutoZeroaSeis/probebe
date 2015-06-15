c28d36b6de0bafe56c98653f8ac1b8e007ec2a1f
cartolari <bruno.cartolari@gmail.com>
2015-06-15 06:07:46 -0300

    Run cron jobs through CLi runner instead of curl

--- app/controllers/admin/message_deliveries_controller.rb
+++ app/controllers/admin/message_deliveries_controller.rb
@@ -5,24 +5,4 @@ class Admin::MessageDeliveriesController < Admin::AdminController
   def table_items
     MessageDeliveries::MessageDelivery.includes(:message, :child, :profile)
   end
-
-  def create
-    date = permitted_params[:delivery_date]
-    testing_mode = permitted_params[:message_for_test]
-
-    MessageSenderWorker.perform_async(date, testing_mode)
-
-    flash[:notice] = "Mensagens Enviadas!"
-    redirect_to admin_message_deliveries_path
-  end
-
-  protected
-
-  def permitted_params
-    if params[:message_deliveries_message_delivery]
-      params.require(:message_deliveries_message_delivery).permit(:delivery_date, :message_for_test)
-    else
-      {}
-    end
-  end
 end
