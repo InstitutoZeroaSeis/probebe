b5c37a050e73e9a62aca97d5e6f85df52512f2aa
diegonakamashi <diegonakamashi@gmail.com>
2015-07-03 16:04:11 -0300

    [Deploy][Staging]

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,4 +3,5 @@ set :job_template, "bash -c ':job'"
 every [:monday, :wednesday, :friday],  at: '11am' do
   runner 'MessageSenderWorker.perform_async(Date.today)'
   command 'echo "Running cron at $(date)" >> /app/log/cron.log '
+
 end
