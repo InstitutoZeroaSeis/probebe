de6b4739bb75f6f0931e7e161151024fd46b4f9c
diegonakamashi <diegonakamashi@gmail.com>
2015-07-07 14:53:24 -0300

    [Blog] - increase font weight

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,4 @@ set :job_template, "bash -c ':job'"
 every [:monday, :wednesday, :friday],  at: '11am' do
   runner 'MessageSenderWorker.perform_async(Date.today)'
   command 'echo "Running cron at $(date)" >> /app/log/cron.log '
-  command 'curl -X POST http://localhost/api/d3d4b74ea38c163c820cd84b25f5/a8eecbf2d604ff6769fd64f1a491 >> /app/log/cron.log'
 end
