d4380cff0b14434d5cc37edf097abab23027ea94
diegonakamashi <diegonakamashi@gmail.com>
2015-08-03 19:13:13 -0300

    Cron test

--- app/workers/message_sender_worker.rb
+++ app/workers/message_sender_worker.rb
@@ -2,11 +2,7 @@ class MessageSenderWorker
   include Sidekiq::Worker
 
   def perform(date, testing_mode = false)
-    system_date = MessageDeliveries::SystemDate.new(date)
-    message_delivery_creator =
-      MessageDeliveries::MessageDeliveryCreator.new(system_date, testing_mode: testing_mode)
-    message_delivery_creator.create_deliveries_for_all_children
-    MessageDeliveryFinderWorker.perform_async
+    Rails.logger.info "WORKER - Cron test"
   end
 
 end
