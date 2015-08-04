eadf5feac0eee9cf31261ebeeef00a5147c23206
diegonakamashi <diegonakamashi@gmail.com>
2015-08-04 19:49:31 -0300

    [Deploy] cron using curl

--- app/workers/message_sender_worker.rb
+++ app/workers/message_sender_worker.rb
@@ -2,7 +2,11 @@ class MessageSenderWorker
   include Sidekiq::Worker
 
   def perform(date, testing_mode = false)
-    Rails.logger.info "TESTE CRON"
+    system_date = MessageDeliveries::SystemDate.new(date)
+    message_delivery_creator =
+      MessageDeliveries::MessageDeliveryCreator.new(system_date, testing_mode: testing_mode)
+    message_delivery_creator.create_deliveries_for_all_children
+    MessageDeliveryFinderWorker.perform_async
   end
 
 end
