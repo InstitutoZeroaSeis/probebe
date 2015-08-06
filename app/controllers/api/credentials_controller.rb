23b1e01554afa2cef657cd828a59588d94e20e9e
diegonakamashi <diegonakamashi@gmail.com>
2015-08-06 15:11:01 -0300

    Return profile type on mobile login

--- app/controllers/api/credentials_controller.rb
+++ app/controllers/api/credentials_controller.rb
@@ -3,7 +3,14 @@ class Api::CredentialsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    valid_hash = { valid: user.try(:valid_password?, params[:password]) || false }
+
+    if user.present?
+      valid_hash = { valid: user.try(:valid_password?, params[:password]) || false }
+      valid_hash[:profile_type] = user.profile.profile_type if valid_hash[:valid]
+    else
+      valid_hash = { valid: false }
+    end
+
     render json: valid_hash
   end
 end
