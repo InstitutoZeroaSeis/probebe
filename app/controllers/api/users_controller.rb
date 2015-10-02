daaef573f2daa308cf521a87e92f862a77dbe2b5
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2015-10-02 10:35:21 -0300

    API to signup from app

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < Devise::RegistrationsController
-  include HeaderAuthenticationConcern
-
+  # include HeaderAuthenticationConcern
+  skip_before_action :verify_authenticity_token
   protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters
   respond_to :json
