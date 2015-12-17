55a8e7650a3d404085a39119ed1b2e2c11d9a594
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2015-12-17 15:07:29 -0200

    Add child age in week at delivery

--- app/serializers/message_delivery_serializer.rb
+++ app/serializers/message_delivery_serializer.rb
@@ -1,7 +1,7 @@
 class MessageDeliverySerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :text, :delivery_date, :url
+  attributes :id, :text, :delivery_date, :url, :child_age_in_week_at_delivery
 
   def text
     object.text
