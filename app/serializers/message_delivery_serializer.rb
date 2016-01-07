ad0e2ccfad41d48da2a0f0309b3cc919842b102d
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-01-07 18:10:56 -0200

    Add properties of article on message delivery

--- app/serializers/message_delivery_serializer.rb
+++ app/serializers/message_delivery_serializer.rb
@@ -1,7 +1,7 @@
 class MessageDeliverySerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :text, :delivery_date, :url, :child_age_in_week_at_delivery
+  attributes :id, :text, :delivery_date, :url, :child_age_in_week_at_delivery, :article_category, :article_text, :article_title
 
   def text
     object.text
@@ -10,6 +10,22 @@ class MessageDeliverySerializer < ActiveModel::Serializer
   def url
     if object.article.try(:publishable?)
       article_url(object.article)
+    end
+  end
+
+  def article_category
+    object.message.category.name
+  end
+
+  def article_text
+    if object.article.try(:publishable?)
+      object.article.text
+    end
+  end
+
+  def article_title
+    if object.article.try(:publishable?)
+      object.article.title
     end
   end
 end
