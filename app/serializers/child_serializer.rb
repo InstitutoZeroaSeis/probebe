07529faeb035c03309bde1156b11e18b992de815
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-01-14 08:38:41 -0200

    Define limit to message deliveries at API

--- app/serializers/child_serializer.rb
+++ app/serializers/child_serializer.rb
@@ -2,4 +2,8 @@ class ChildSerializer < ActiveModel::Serializer
   attributes :id, :name, :gender, :birth_date, :age_in_weeks, :pregnancy?
   has_many :message_deliveries, root: :messages, serializer: MessageDeliverySerializer
 
+
+  def message_deliveries
+    object.message_deliveries.order(delivery_date: :desc).limit(15)
+  end
 end
