86758cd68e80a54440c1022e01a8feb66a29264b
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-01-20 15:09:06 -0200

    4pm cron messages

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment")
-every [:monday, :wednesday, :friday],  at: '11am' do
+every [:monday, :wednesday, :friday],  at: '4pm' do
   url = Rails.application.config.server_url
   command "curl -X POST #{url}/api/d3d4b74ea38c163c820cd84b25f5/a8eecbf2d604ff6769fd64f1a492 >> /app/log/cron.log"
   #runner 'MessageSenderWorker.perform_async(Date.today)'
