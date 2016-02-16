7eb5528f6e0e7151837d6e1dc4db0ae982a2c635
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-02-16 18:00:42 -0200

    Add validates

--- app/models/site/menu.rb
+++ app/models/site/menu.rb
@@ -5,12 +5,22 @@ class Site::Menu < ActiveRecord::Base
   belongs_to :page, class_name: 'Page'
   has_many :submenus, foreign_key: :parent_menu_id
 
+  validates :name, :position, presence: true
+  validate :page_or_link
+
   def self.base_menus
     where(parent_menu_id: nil).order(:position)
   end
 
   def self.submenus_from(parent_id)
     where(parent_menu_id: parent_id).order(:position)
+  end
+
+  def page_or_link
+    if !self.link.present? && !self.page.present? && self.parent_menu.present?
+      errors.add(:base, :has_no_link_or_page)
+      return false
+    end
   end
 
 end
--- app/models/site/page.rb
+++ app/models/site/page.rb
@@ -5,6 +5,8 @@ class Site::Page < ActiveRecord::Base
   has_one :menu, class_name: 'Menu'
   friendly_id :title, use: :slugged
 
+  validates :title, :text, presence: true
+
   def label
     self.title
   end
