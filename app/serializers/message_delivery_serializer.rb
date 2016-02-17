430116349f0745b935fd7fbcbf08209025027e40
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-02-17 16:32:12 -0200

    Add more attributes to message delivery serializer

--- app/serializers/message_delivery_serializer.rb
+++ app/serializers/message_delivery_serializer.rb
@@ -2,7 +2,7 @@ class MessageDeliverySerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id, :text, :delivery_date, :url, :child_age_in_week_at_delivery, :article_category,
-            :article_text, :article_title, :mon_is_pregnant, :category_id
+            :article_text, :article_title, :mon_is_pregnant, :parent_category_id, :category_id, :category_color
 
   def text
     object.text
@@ -36,5 +36,15 @@ class MessageDeliverySerializer < ActiveModel::Serializer
 
   def category_id
     object.message.category.id
+  end
+
+  def parent_category_id
+    object.message.category.parent_category.id
+  end
+
+  def category_color
+    category = object.message.category
+    return category.color unless category.parent_category.present?
+    category.parent_category.color
   end
 end
