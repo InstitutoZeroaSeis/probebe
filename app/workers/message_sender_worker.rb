9af456759aa5825c6fff7bbd1f56065228a4e1e1
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-03-08 13:25:31 -0300

    Refactoring to send by child

--- app/workers/message_sender_worker.rb
+++ app/workers/message_sender_worker.rb
@@ -1,16 +1,15 @@
 class MessageSenderWorker
   include Sidekiq::Worker
 
-  def perform(date, testing_mode = false)
-    managerMessage = MessageDeliveries::ManagerMessageDeliveries.create(messages_created_start: Time.now)
+  def perform(date, child_id, testing_mode = false)
+    child = Child.find(child_id)
 
     system_date = MessageDeliveries::SystemDate.new(date)
     message_delivery_creator =
       MessageDeliveries::MessageDeliveryCreator.new(system_date, testing_mode: testing_mode)
-    message_delivery_creator.create_deliveries_for_all_children
 
-    managerMessage.update(messages_created_end: Time.now, sum_messages_created: MessageDeliveries::MessageDelivery.created_today.size)
-    MessageDeliveryFinderWorker.perform_async(managerMessage.id)
+    message_delivery = message_delivery_creator.create_delivery_for(child)
+    MessageDeliveries::MessageDeliveryFinder.find_and_deliver_messages(message_delivery) if message_delivery.present?
   end
 
 end
