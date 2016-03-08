e3857192f725ec739ab907f696001f032444215e
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-03-08 17:22:40 -0300

    Get child with only valid age in weeks

--- app/controllers/api/message_deliveries_controller.rb
+++ app/controllers/api/message_deliveries_controller.rb
@@ -2,8 +2,10 @@ class Api::MessageDeliveriesController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    managerMessage = MessageDeliveries::ManagerMessageDeliveries.create(messages_created_start: Time.now)
-    Child.completed_profile.map do |child|
+    children = Child.completed_profile.select { |child| child.valid_age_in_weeks? }
+    managerMessage = MessageDeliveries::ManagerMessageDeliveries.create(messages_created_start: Time.now, sum_messages_created: children.size)
+
+    children.map do |child|
       MessageSenderWorker.perform_async(Date.today, child.id)
     end
     managerMessage.update(creator_jobs_end: Time.now)
