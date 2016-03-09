3952f526961612fcbf54377ae5f61e515cdfded6
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-03-09 17:20:55 -0300

    Fixing tests from MessageDeliveries::MessageDeliveryCreator

--- spec/models/message_deliveries/message_delivery_creator_spec.rb
+++ spec/models/message_deliveries/message_delivery_creator_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe MessageDeliveries::MessageDeliveryCreator, type: :model do
     @profile = create(:profile, :without_children)
   end
 
-  describe '.create_deliveries_for_all_children' do
+  describe '.create_delivery_for' do
     before { @child = create(:child, birth_date: 5.months.ago, profile: @profile) }
     before { @message = create(:message, :with_article, :male, maximum_valid_week: 22, baby_target_type: 'born') }
-    subject { MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_deliveries_for_all_children }
+    subject { MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_delivery_for(@child) }
     it 'is expected to correctly create the message delivery' do
-      expect(subject.first.message).to eq(@message)
-      expect(subject.first.child).to eq(@child)
-      expect(subject.first.message_for_test).to be false
-      expect(subject.first.cell_phone_number).to eq(@child.primary_cell_phone_number)
-      expect(subject.first.device_registrations).to eq(@child.device_registrations)
-      expect(subject.first.sms_allowed).to eq(@child.authorized_receive_sms)
-      expect(subject.first.status).to eq('not_sent')
+      expect(subject.message).to eq(@message)
+      expect(subject.child).to eq(@child)
+      expect(subject.message_for_test).to be false
+      expect(subject.cell_phone_number).to eq(@child.primary_cell_phone_number)
+      expect(subject.device_registrations).to eq(@child.device_registrations)
+      expect(subject.sms_allowed).to eq(@child.device_registrations.empty?)
+      expect(subject.status).to eq('not_sent')
     end
   end
 
@@ -27,7 +27,7 @@ RSpec.describe MessageDeliveries::MessageDeliveryCreator, type: :model do
     before { MessageDeliveries::MessageDelivery.create!(child: @child, message: @message) }
     before { @before_message_count = MessageDeliveries::MessageDelivery.count }
 
-    subject { MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_deliveries_for_all_children }
+    subject { MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_delivery_for(@child) }
 
     it 'is expected to create no additional messages' do
       expect(MessageDeliveries::MessageDelivery.count).to eq(@before_message_count)
@@ -38,7 +38,7 @@ RSpec.describe MessageDeliveries::MessageDeliveryCreator, type: :model do
     before { @child = create(:child, profile: @profile, birth_date: 5.months.ago) }
     before { create(:message, :with_article, :male, maximum_valid_week: 10, baby_target_type: 'pregnancy') }
 
-    subject { MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_deliveries_for_all_children }
+    subject { MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_delivery_for(@child) }
 
     it 'is expected to deliver no messages' do
       expect(MessageDeliveries::MessageDelivery.count).to eq(0)
@@ -50,7 +50,7 @@ RSpec.describe MessageDeliveries::MessageDeliveryCreator, type: :model do
       before { @child = create(:child, profile: @profile, birth_date: (Child::PREGNANCY_DURATION_IN_WEEKS.weeks - 5.months).from_now) }
       before { @message = create(:message, :with_article, :male, maximum_valid_week: 24, baby_target_type: 'pregnancy') }
       it 'is expected to send a message for the given child' do
-        MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_deliveries_for_all_children
+        MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_delivery_for(@child)
         expect(MessageDeliveries::MessageDelivery.count).to eq(1)
         expect(MessageDeliveries::MessageDelivery.first.message).to eq(@message)
         expect(MessageDeliveries::MessageDelivery.first.child).to eq(@child)
@@ -62,27 +62,16 @@ RSpec.describe MessageDeliveries::MessageDeliveryCreator, type: :model do
     before { @child = create(:child, birth_date: 5.months.ago, profile: @profile) }
     before { @child2 = create(:child, birth_date: 5.months.ago, profile: @profile) }
     before { @message = create(:message, :with_article, :male, maximum_valid_week: 22, baby_target_type: 'born') }
-    subject { MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_deliveries_for_all_children }
+    subject { MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_delivery_for(@child) }
     it 'is expected to correctly create the message delivery' do
-      expect(subject.first.message).to eq(@message)
-      expect(subject.first.child).to eq(@child)
-      expect(subject.first.message_for_test).to be false
-      expect(subject.first.cell_phone_number).to eq(@child.primary_cell_phone_number)
-      expect(subject.first.device_registrations).to eq(@child.device_registrations)
-      expect(subject.first.sms_allowed).to eq(@child.authorized_receive_sms)
-      expect(subject.first.status).to eq('not_sent')
+      expect(subject.message).to eq(@message)
+      expect(subject.child).to eq(@child)
+      expect(subject.message_for_test).to be false
+      expect(subject.cell_phone_number).to eq(@child.primary_cell_phone_number)
+      expect(subject.device_registrations).to eq(@child.device_registrations)
+      expect(subject.sms_allowed).to eq(@child.device_registrations.empty?)
+      expect(subject.status).to eq('not_sent')
     end
   end
 
-  context 'when the child has a donor' do
-    before { @donor = create(:profile) }
-    before { @child = create(:child, profile: @profile, donor: @donor, birth_date: (Child::PREGNANCY_DURATION_IN_WEEKS.weeks - 5.months).from_now) }
-    before { @message = create(:message, :with_article, :male, maximum_valid_week: 24, baby_target_type: 'pregnancy') }
-
-    it "should create a donated_message" do
-      MessageDeliveries::MessageDeliveryCreator.new(@system_date).create_deliveries_for_all_children
-        expect(MessageDeliveries::MessageDelivery.count).to eq(1)
-        expect(MessageDeliveries::DonatedMessage.count).to eq(1)
-    end
-  end
 end
