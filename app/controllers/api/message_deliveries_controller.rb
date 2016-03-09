6e39f7d544dfb3e97302b8e377eb6f4c95b9cb13
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-03-09 16:00:38 -0300

    Fixing lock to manager message deliveries

--- app/controllers/api/message_deliveries_controller.rb
+++ app/controllers/api/message_deliveries_controller.rb
@@ -5,7 +5,7 @@ class Api::MessageDeliveriesController < ApplicationController
     children = Child.completed_profile.select { |child| child.valid_age_in_weeks? }
     managerMessage = MessageDeliveries::ManagerMessageDeliveries.create(messages_created_start: Time.now, sum_messages_created: children.size)
 
-    children.map do |child|
+    children.each do |child|
       MessageSenderWorker.perform_async(Date.today, child.id)
     end
     head :ok
