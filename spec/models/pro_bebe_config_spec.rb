433cebd52a3106bf67d1bf1ba4b1f0bb8745ded1
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-03-10 13:37:34 -0300

    Define test to elasticsearch host at probebe_config

--- spec/models/pro_bebe_config_spec.rb
+++ spec/models/pro_bebe_config_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe ProBebeConfig, type: :model do
         "wsdl_name_space" => "http://namespace.example.com",
         "spring_user" => "user",
         "spring_password" => "password",
-        "elasticsearch" => {host: 'elasticsearch:9200'}
-      }
+      },
+      "elasticsearch" => {host: 'elasticsearch:9200'}
     }
     stub_const "ProBebeConfig::APP_CONFIG", @app_config_hash
   end
@@ -24,5 +24,5 @@ RSpec.describe ProBebeConfig, type: :model do
   its(:wsdl_name_space) { should eq(@app_config_hash["message_delivery"]["wsdl_name_space"]) }
   its(:spring_user) { should eq(@app_config_hash["message_delivery"]["spring_user"]) }
   its(:spring_password) { should eq(@app_config_hash["message_delivery"]["spring_password"]) }
-  its(:elasticsearch_host) { should eq(@app_config_hash["message_delivery"]["elasticsearch"]["host"]) }
+  its(:elasticsearch_host) { should eq(@app_config_hash["elasticsearch"]["host"]) }
 end
