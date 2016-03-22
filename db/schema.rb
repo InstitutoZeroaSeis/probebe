087b8dace143e2cc3ee260aaaadc75f49ad39b18
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-03-22 10:31:22 -0300

    Unathorize receive sms to user with devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309184404) do
+ActiveRecord::Schema.define(version: 20160322131443) do
 
   create_table "article_references", force: true do |t|
     t.string   "source"
