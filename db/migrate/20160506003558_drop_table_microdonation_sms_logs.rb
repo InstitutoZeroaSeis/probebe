ad15f3365e5d127082ae02eb35a0a9bf28e2ea9a
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-05-06 19:11:40 -0300

    Drop table if exist microdonation sms logs

--- db/migrate/20160506003558_drop_table_microdonation_sms_logs.rb
+++ db/migrate/20160506003558_drop_table_microdonation_sms_logs.rb
@@ -1,5 +1,5 @@
 class DropTableMicrodonationSmsLogs < ActiveRecord::Migration
   def change
-    drop_table :microdonation_sms_logs
+    drop_table :microdonation_sms_logs if ActiveRecord::Base.connection.table_exists? 'microdonation_sms_logs'
   end
 end
