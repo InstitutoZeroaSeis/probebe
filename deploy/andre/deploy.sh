53c060322d450211573e4a0c0d57a32e88718c42
Antonio Anderson Souza <antonioams@gmail.com>
2016-06-03 13:16:44 +0000

    Adicionado outro ambiente de deploy

---
+++ deploy/andre/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+APP_NAME='Probebe'
+ENV_NAME='probebe-env'
+APP_VERSION=staging-$(git rev-parse --short HEAD)
+APP_VERSION_DESCRIPTION=$(git log -1 --pretty=%B)
+S3_BUCKET='elasticbeanstalk-us-east-1-567541957573'
+FILE_NAME=deploy-${APP_VERSION}.zip
+
+
+echo "Building Docker Image..."
+docker build -t vizir/probebe:staging -f docker/web_staging/Dockerfile .
+echo "Pushing Docker Image..."
+docker push vizir/probebe:staging
+
+echo "Creating deploy bundle"
+[[ -f tmp/deploy.zip ]] && rm -f tmp/deploy.zip
+zip tmp/deploy.zip .ebextensions/* Dockerrun.staging.aws.json
+printf "@ Dockerrun.staging.aws.json\n@=Dockerrun.aws.json\n" | zipnote -w tmp/deploy.zip
+
+echo "Uploading to S3"
+aws s3 cp tmp/deploy.zip s3://"$S3_BUCKET"/"$FILE_NAME"
+
+echo "Registrating uploaded file as a Beanstalk version"
+aws elasticbeanstalk create-application-version --application-name $APP_NAME \
+  --description "$APP_VERSION_DESCRIPTION" \
+  --version-label "$APP_VERSION" \
+  --source-bundle "S3Bucket=$S3_BUCKET,S3Key=$FILE_NAME"
+
+echo "Deploy to Beanstalk"
+aws elasticbeanstalk update-environment \
+  --environment-name "$ENV_NAME" \
+  --version-label "$APP_VERSION"
