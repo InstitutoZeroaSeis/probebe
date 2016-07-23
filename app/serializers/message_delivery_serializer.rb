8f585814a2d3b2660ab5a6080bc7dcde8f178611
Matheus Portillo <mathportillo@gmail.com>
2016-07-23 18:21:06 -0300

    added article cover picture to message delivery serializer

--- app/serializers/message_delivery_serializer.rb
+++ app/serializers/message_delivery_serializer.rb
@@ -2,7 +2,8 @@ class MessageDeliverySerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id, :text, :delivery_date, :url, :child_age_in_week_at_delivery, :article_category,
-            :article_text, :article_title, :mon_is_pregnant, :parent_category_id, :category_id, :category_color
+            :article_text, :article_title, :mon_is_pregnant, :parent_category_id, :category_id, :category_color,
+            :article_cover_picture
 
   def text
     object.text
@@ -46,5 +47,9 @@ class MessageDeliverySerializer < ActiveModel::Serializer
     category = object.message.category
     return category.color unless category.parent_category.present?
     category.parent_category.color
+  end
+
+  def article_cover_picture
+    object.message.article.cover
   end
 end
